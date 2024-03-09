976c11800440fa3971d58373ca2642f9ec8943e1
MedantSharan <medsharanofficial@gmail.com>
2024-03-09 16:51:31 +0000

    Models for purpose added

--- NotForgot/tasks/models.py
+++ NotForgot/tasks/models.py
@@ -18,25 +18,37 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True, blank=False)
 
-
     class Meta:
         """Model options."""
-
         ordering = ['last_name', 'first_name']
 
     def full_name(self):
         """Return a string containing the user's full name."""
-
         return f'{self.first_name} {self.last_name}'
 
     def gravatar(self, size=120):
         """Return a URL to the user's gravatar."""
-
         gravatar_object = Gravatar(self.email)
         gravatar_url = gravatar_object.get_image(size=size, default='mp')
         return gravatar_url
 
     def mini_gravatar(self):
         """Return a URL to a miniature version of the user's gravatar."""
-        
         return self.gravatar(size=60)
+
+class Place(models.Model):
+    """Model to represent a place."""
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    """Model to represent an item associated with a place."""
+    name = models.CharField(max_length=100)
+    place = models.ForeignKey(Place, related_name='items', on_delete=models.CASCADE)
+    forget_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
